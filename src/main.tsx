0b9a0f6005ad6633f2b773ed2a0b54aff62c5f46
Alex Uribe <repzyyog@gmail.com>
2025-05-06 20:15:51 +0200

    Create App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./components/App/App";
+import "@fontsource-variable/inter/index.css";
+import "@fontsource-variable/roboto-slab/index.css";
 import "./styles/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className="main-container"></div>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 );
