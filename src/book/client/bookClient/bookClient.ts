33a5054b65499a579dd219aa7bab8b182aaa3b5b
Alex Uribe <repzyyog@gmail.com>
2025-05-09 17:27:22 +0200

    Create bookClient with getBooks method

---
+++ src/book/client/bookClient/bookClient.ts
@@ -0,0 +1,26 @@
+import { transformBooksInfoDtoToBooksInfo } from "../../dto/transformers";
+import type { BookClientStructure, BooksInfo, BooksInfoDto } from "./types";
+
+class BookClient implements BookClientStructure {
+  private apiUrl = import.meta.env.VITE_API_URL;
+
+  public getBooks = async (pageNumber?: number): Promise<BooksInfo> => {
+    const fetchUrl = !pageNumber
+      ? `${this.apiUrl}/books`
+      : `${this.apiUrl}/books?page=${pageNumber}`;
+
+    const response = await fetch(fetchUrl);
+
+    if (!response.ok) {
+      throw new Error("Error fetching books");
+    }
+
+    const booksInfoDto = (await response.json()) as BooksInfoDto;
+
+    const booksInfo = transformBooksInfoDtoToBooksInfo(booksInfoDto);
+
+    return booksInfo;
+  };
+}
+
+export default BookClient;
