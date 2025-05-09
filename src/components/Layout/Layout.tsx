73d65a0a976153d67aa934889068124c2356916e
Alex Uribe <repzyyog@gmail.com>
2025-05-09 10:26:24 +0200

    Add Navigation to Layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,13 @@
 import type React from "react";
 import Header from "../Header/Header";
+import Navigation from "../Navigation/Navigation";
 import "./Layout.css";
 
 const Layout: React.FC = () => {
   return (
     <div className="main-container">
       <Header />
+      <Navigation />
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
 import App from "./components/App/App";
 import "@fontsource-variable/inter/index.css";
 import "@fontsource-variable/roboto-slab/index.css";
@@ -7,6 +8,8 @@ import "./styles/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
