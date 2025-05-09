2460c1d8b42d54c5138d78038183830814be2041
Alex Uribe <repzyyog@gmail.com>
2025-05-09 20:03:45 +0200

    Create NotFoundPage component with styles

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,18 @@
+import type React from "react";
+import "./NotFoundPage.css";
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <div className="not-found">
+      <img
+        src="/Not-found-page-icon.svg"
+        alt="Book with a magnified glass showing a 404"
+        height={154}
+        width={169.23}
+      />
+      <h2 className="not-found__title">Page Not Found</h2>
+    </div>
+  );
+};
+
+export default NotFoundPage;
