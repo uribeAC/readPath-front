3532a14535b9d53868e5e415983e4882aebd3075
Alex Uribe <repzyyog@gmail.com>
2025-05-11 00:09:41 +0200

    Add BookCard to Bookshelf component

--- src/book/components/Bookshelf/Bookshelf.tsx
+++ src/book/components/Bookshelf/Bookshelf.tsx
@@ -1,5 +1,7 @@
 import type React from "react";
 import type { Book } from "../../types";
+import BookCard from "../BookCard/BookCard";
+import "./Bookshelf.css";
 
 interface BookshelfProps {
   books: Book[];
@@ -9,7 +11,9 @@ const Bookshelf: React.FC<BookshelfProps> = ({ books }) => {
   return (
     <ul className="bookshelf">
       {books.map((book) => (
-        <li key={book.id}></li>
+        <li key={book.id}>
+          <BookCard book={book} />
+        </li>
       ))}
     </ul>
   );
