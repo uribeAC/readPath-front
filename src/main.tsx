19efd12d4e2767c1ede8c009be936a7ec533d2d7
Alex Uribe <repzyyog@gmail.com>
2025-05-10 13:15:41 +0200

    Reduce size of svg images

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import { Provider } from "react-redux";
+import store from "./store/store";
 import AppRouter from "./router/AppRouter";
 import "@fontsource-variable/inter/index.css";
 import "@fontsource-variable/roboto-slab/index.css";
 import "./styles/styles.css";
-import { Provider } from "react-redux";
-import store from "./store/store";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
