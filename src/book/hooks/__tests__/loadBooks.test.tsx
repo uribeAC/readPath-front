bca54ff978f5499322447ec006f82ce08502df61
Alex Uribe <repzyyog@gmail.com>
2025-05-10 12:44:16 +0200

    Add test to loadBooks method of useBooks

---
+++ src/book/hooks/__tests__/loadBooks.test.tsx
@@ -0,0 +1,41 @@
+import { renderHook } from "@testing-library/react";
+import useBooks from "../useBooks";
+import { act } from "react";
+import { Provider } from "react-redux";
+import store from "../../../store/store";
+
+describe("Given the loadBooks function", () => {
+  describe("When it's called with page number 2", () => {
+    test("Then it should set the book Vinland Saga Vol. 1 and Demon Slayer: Kimetsu no Yaiba Vol. 1 as booksInfo with a total of 12", async () => {
+      const expectedKimetsuTitle = "Demon Slayer: Kimetsu no Yaiba Vol. 1";
+      const expectedVinlandTitle = "Vinland Saga Vol. 1";
+      const expectedBooksTotal = 12;
+
+      const wrapper = ({ children }: { children: React.ReactNode }) => (
+        <Provider store={store}>{children}</Provider>
+      );
+
+      const { result } = renderHook(() => useBooks(), { wrapper });
+
+      await act(() => {
+        result.current.loadBooks(2);
+      });
+
+      const books = result.current.books.books;
+      const totals = result.current.books.totals;
+
+      expect(books).toContainEqual(
+        expect.objectContaining({
+          title: expectedKimetsuTitle,
+        }),
+      );
+      expect(books).toContainEqual(
+        expect.objectContaining({
+          title: expectedVinlandTitle,
+        }),
+      );
+
+      expect(totals.books).toBe(expectedBooksTotal);
+    });
+  });
+});
--- src/book/mocks/handlers.ts
+++ src/book/mocks/handlers.ts
@@ -9,7 +9,21 @@ if (!apiUrl) {
 }
 
 export const handlers = [
-  http.get(`${apiUrl}/books`, () => {
+  http.get(`${apiUrl}/books`, ({ request }) => {
+    const url = new URL(request.url);
+    const currentPage = url.searchParams.get("page");
+
+    if (currentPage === "2") {
+      return HttpResponse.json<BooksInfoDto>({
+        books: mangaFixtures.slice(10, 20),
+        totals: {
+          books: 12,
+          booksRead: 7,
+          booksToRead: 5,
+        },
+      });
+    }
+
     return HttpResponse.json<BooksInfoDto>({
       books: mangaFixtures.slice(0, 10),
       totals: {
