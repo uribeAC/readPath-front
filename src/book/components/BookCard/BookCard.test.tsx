440a5a024254945a35a87ba1638b07818e0a1e3d
Alex Uribe <repzyyog@gmail.com>
2025-05-11 00:21:49 +0200

    Add tests to BookCard component

---
+++ src/book/components/BookCard/BookCard.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import { narutoBook } from "../../fixtures/fixtures";
+import BookCard from "./BookCard";
+
+describe("Given the BookCard component", () => {
+  describe("When it receives Naruto Vol. 1 book", () => {
+    test("Then it should show Naruto Vol. 1 inside a heading", () => {
+      const expectedTitle = /naruto vol. 1/i;
+
+      render(<BookCard book={narutoBook} />);
+
+      const bookTitle = screen.getByRole("heading", { name: expectedTitle });
+
+      expect(bookTitle).toBeInTheDocument();
+    });
+
+    test("Then it should show an image of Naruto Volume 1 Cover", () => {
+      render(<BookCard book={narutoBook} />);
+
+      const bookImage = screen.getByAltText(narutoBook.imageAlt);
+
+      expect(bookImage).toBeInTheDocument();
+    });
+
+    test("Then it should show a 4 star rating", () => {
+      const expectedLabel = /4 star rating/i;
+
+      render(<BookCard book={narutoBook} />);
+
+      const ratingStars = screen.getByLabelText(expectedLabel);
+
+      expect(ratingStars).toBeInTheDocument();
+    });
+  });
+});
--- src/book/fixtures/fixtures.ts
+++ src/book/fixtures/fixtures.ts
@@ -2,3 +2,5 @@ import { transformBooksDtoToBooks } from "../dto/transformers";
 import { shonenFixturesDto } from "./fixturesDto";
 
 export const shonenFixtures = transformBooksDtoToBooks(shonenFixturesDto);
+
+export const narutoBook = shonenFixtures[0];
