e81c8a6b2d4dbd134014c5cbf905584b82380f38
Alex Uribe <repzyyog@gmail.com>
2025-05-13 19:15:20 +0200

    Create Loading component with styles

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,18 @@
+import type React from "react";
+import "./Loading.css";
+
+const Loading: React.FC = () => {
+  return (
+    <div className="loading">
+      <h2 className="loading__text loading__text--animation">
+        Loading information . . .
+      </h2>
+      <img src="/loading-icon.webp" alt="" />
+      <span className="loading__text loading__text--underline">
+        Your bookshelf is getting sorted.
+      </span>
+    </div>
+  );
+};
+
+export default Loading;
