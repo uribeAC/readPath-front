51c90a98d7ec9a4fc19bda98b3e8e36bde1f2a20
Alex Uribe <repzyyog@gmail.com>
2025-05-18 19:35:03 +0200

    Add BookDetailPage to AppRouter

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router";
 import App from "../components/App/App";
 import {
   LazyAddBookPage,
+  LazyBookDetailPage,
   LazyBooksPage,
   LazyNotFoundPage,
 } from "./components/LazyLoaders";
@@ -13,6 +14,7 @@ const AppRouter: React.FC = () => {
         <Route index element={<Navigate to="/books" />} />
         <Route path="books" element={<LazyBooksPage />} />
         <Route path="add-book" element={<LazyAddBookPage />} />
+        <Route path="book/:bookId" element={<LazyBookDetailPage />} />
         <Route path="*" element={<LazyNotFoundPage />} />
       </Route>
     </Routes>
